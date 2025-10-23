e132feb079d2dda8f580fe21dfb4eacdb12002c8
zer0tres <109388900+zer0tres@users.noreply.github.com>
2025-10-23 00:50:25 +0000

    adiciona script de variáveis com prompts para coleta de dados do usuário

---
+++ AULAS/javascript/variaveis.js
@@ -0,0 +1,31 @@
+// window.alert('ok') // mostra um pop
+// console.log('Meu nome não é Johi') // mostra no console f12
+
+/*     comentario de bloco, inutiliza o q esta dentro
+variaveis sao espacos em memoria usados para armazenar dados reaproveitaveis.
+
+arrays 
+
+    var - desaconselhavel
+    let - valor que podera variar
+    const - valor que nao deve variar
+
+*/
+
+
+let nome = prompt("qual seu nome? ")  // nome é igual a ledson (isto é atribuicao)
+let sobrenome = prompt ("Qual é seu sobrenome?")
+const cpf = prompt ("Qual seu cpf [somente numeros]?")
+const solteiro = true
+let roupa = prompt ("Qual seu estilo de roupa?")
+
+console.log("------PERFIL -----\n Nome:") // n de enter 
+console.log("Nome: " + nome)
+console.log("sobrenome: " + nome)
+console.log("cpf: " + nome)
+console.log("solteiro: " + nome)
+console.log("roupa: " + nome)
+console.log("-----------\n :")
+
+//window.alert(nome + sobrenome)
+//console.log(nome + " " + sobrenome) // o sinal mais e usado para concatenar valores
