1073e3564d6b947df178712a0b50253580b0c3a2
zer0tres <109388900+zer0tres@users.noreply.github.com>
2025-10-29 23:41:09 +0000

    atualiza a porta do live server para 5502 e adiciona o arquivo 'funcoes.js' com comentários sobre funções em JavaScript

---
+++ AULAS/javascript/funcoes.js
@@ -0,0 +1,8 @@
+// Funçoes sao blliocos de codigo que podem ser
+// reaproveitados ao longo da execucao do programa
+// caracteristicas:
+//   - podem ser nomeados ou não
+//   - podem receber parametros ou não
+//   - podem retornar valores ou não
+
+
