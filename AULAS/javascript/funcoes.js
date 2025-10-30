0adb72562b9b2f673428eeb1e9c662632ee1bea0
zer0tres <109388900+zer0tres@users.noreply.github.com>
2025-10-30 00:21:49 +0000

    corrige erro de digitação na descrição das funções em 'funcoes.js'

--- AULAS/javascript/funcoes.js
+++ AULAS/javascript/funcoes.js
@@ -1,4 +1,4 @@
-// Funçoes sao blliocos de codigo que podem ser
+// Funçoes sao blocos de codigo que podem ser
 // reaproveitados ao longo da execucao do programa
 // caracteristicas:
 //   - podem ser nomeados ou não
@@ -9,3 +9,19 @@
 function dizOla(){
     alert("Diz olá!")
 }
+function dizOlaPessoa(nome){
+    alert(`pode falar, ${nome}!`)
+
+}
+const jogadores = ['Messi', 'Neymar', 'Mbappe', 'Ronaldo']
+const frutas = ['Banana', 'Maçã', 'Uva', 'Abacaxi']
+
+function formataTexto(lista){
+    for(let texto of lista){
+        console.log(texto.toUpperCase())
+        //texto.toLowerCase() > tudo para minuscula
+        //texto.trim() > remove espacos em branco
+        //texto.join("-") > junta os elementos
+    }
+}
+formataTexto(jogadores)
