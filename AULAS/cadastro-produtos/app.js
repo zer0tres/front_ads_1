f99cb3bc8980f3169ff65914f443c6f37140b614
zer0tres <109388900+zer0tres@users.noreply.github.com>
2025-11-12 23:43:50 +0000

    adiciona arquivo 'app.js' com lógica para manipulação de formulário e armazenamento de produtos

---
+++ AULAS/cadastro-produtos/app.js
@@ -0,0 +1,28 @@
+/**ALGORITMO 
+ * 1- Pegar a referências dos dados do Formulário
+ * 2- Armazenar e agrupar os dados em Estrutura de Dados adequada
+ * 3- Criar um Modelo dinâmico de Card
+ * 4- Renderizar o Modelo de Card com os dados armazenados
+ * 5- Apagar os dados da lista (último inserido na lista) 
+ * 
+ * CARD (3 linhas Div com img, h3, p) Classe CSS do card: 'card','imagem-container'
+ * Após cada operação imporante Limpar Cache ou Atualizar Card
+*/
+const formulario = document.querySelector('#form-user')
+const btnRemover = document.querySelector('#remover')
+const containerCards = document.querySelector('.container-cards')
+
+const itensProdutos = [
+    // {nome: "Prod1", descricao: "", url: ""}
+    // {nome: "Prod1", descricao: "", url: ""}
+]
+// addeventlistener => escuta o evento disparado
+formulario.addEventListener('submit', function(event){
+    event.preventDefault() // previne o comportamento padrão do formulário (recarregar a página)
+    const titulo = document.querySelector('#nome')
+    const descricao = document.querySelector('#descricao')
+    const urlImagem = document.querySelector('#img')
+    itensProdutos.push({titulo, descricao, urlImagem})
+    console.log(itensProdutos)
+    formulario.reset() // limpa os campos do formulário
+})
